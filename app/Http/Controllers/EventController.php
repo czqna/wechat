e1e7d5cea1fb80a88a2815a00192e5bc6179662d
unknown <1299313230@qq.com>
2019-10-09 10:30:38 +0800

    添加初始内容

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,14 +15,15 @@ class EventController extends Controller
     }
 	public function index(Request $request){
 		$access_token=$this->wechat->access_token();
-		// dd($access_token);
-		$r=json_decode($access_token,1);
+		dd($access_token);
+		// $r=json_decode($access_token,1);
 		// dd($r);
 		$data=[
 			'access_token'=>$access_token,
 		];
 		$re=DB::table('wechat')->insert($data);
 		dd($re);
+		
 	}
   
 }
--- app/Http/Tool/Wechat.php
+++ app/Http/Tool/Wechat.php
@@ -8,10 +8,10 @@ class Wechat{
 
  public function access_token(){
 
-   	if (Cache::pull('access_token_key')) {
+   	if (Cache::has('access_token_key')) {
    		//有的话去缓存拿
-  		$access_token=Cache::push('access_token');
-  		echo $access_token;
+  		$access_token=Cache::get('access_token_key');
+  		// echo $access_token;
    	}else{
    		//没有通过微信接口拿
    		$access_toke=file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WECHAT_APPID')."&secret=".env('WECHAT_APPSECRET'));
@@ -21,7 +21,7 @@ class Wechat{
    		$access_token=$access_re['access_token'];
    		$expires_in=$access_re['expires_in'];
    		//加入缓存
-   		Cache::put('access_token_key',$access_token,now()->addMinutes($expires_in));
+   		Cache::put('access_token_key',$access_token,$expires_in);
    		// dd($a);
    	}
    	return $access_token;
