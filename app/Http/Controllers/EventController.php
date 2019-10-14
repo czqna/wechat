a6bc46fa3b298ff48eedcc08acda5eb275e8cf6a
unknown <1299313230@qq.com>
2019-10-14 16:04:10 +0800

    aa

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -209,9 +209,9 @@ class EventController extends Controller
   }
   public function event(){
   		$info=file_get_contents("php://input");
-  		file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),data:"<<<<<<<<<<<<<<<<<<",FILE_APPEND);
+  		file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<",FILE_APPEND);
   		file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
-  	
+
 
   }
 
